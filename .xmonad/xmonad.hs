86beb2e393d7ce4abf78481c14c4f591ddc8da56
Vishnu Rajeevan <vishnurajeevan@gmail.com>
2015-04-19 01:55:10 -0400

    font fixes for hidpi

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -115,7 +115,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
 
     -- launch dmenu
-    , ((modm,               xK_p     ), spawn "dmenu_run.pl")
+    , ((modm,               xK_p     ), spawn "dmenu_run.pl -fn \"-*-roboto light-*-r-*-*-*-*-*-*-*-*-*-*\"")
 
     -- launch gmrun
     {-, ((modm .|. shiftMask, xK_p     ), spawn "gmrun")-}
--- touchpad.sh
+++ touchpad.sh
@@ -2,7 +2,7 @@
 touchpad_off=`synclient -l  | grep -c 'TouchpadOff.*=.*0' | head -c 1`
 TOUCHPAD_ON=1
 
-if [ "$touchpad_off" -eq "1" ]; then
+if [ "$touchpad_off" -eq $TOUCHPAD_ON ]; then
     touchpad_string="on"
 else
     touchpad_string="off"
